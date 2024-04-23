b5498f20bc4e5e8c5162c1aec54bf21de127df4a
Jinnan Zhang <vannizhang@gmail.com>
2024-04-23 11:31:21 -0700

    feat: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className="w-full text-center pb-2 mb-4 border-b border-custom-background-1">
+            <h2 className=" text-2xl">Terrestrial Ecosystems of Myanmar</h2>
+        </div>
+    );
+};
--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@components/Header/Header';
 import { Histogram } from '@components/Histogram';
 import { InfoPanel } from '@components/InfoPanel';
 import React from 'react';
@@ -5,6 +6,7 @@ import React from 'react';
 export const SidePanel = () => {
     return (
         <div className="absolute top-0 left-0 bottom-0 w-side-panel-width p-4 overflow-y-auto fancy-scrollbar">
+            <Header />
             <InfoPanel />
             <Histogram />
         </div>
