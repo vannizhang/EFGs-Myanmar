92068a04a2be5845c5a927fae4196943fc34a617
Jinnan Zhang <vannizhang@gmail.com>
2024-04-30 07:31:21 -0700

    chore: clean up contants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,12 +10,6 @@
  */
 export const WEB_MAP_ID = 'c5eca89fd37a4436bdf096f3060254e1'; //'14e065e2fbfa4b10ac1a7aeec5a5dbea';
 
-// /**
-//  * Clone of the webmap above Contains basis to evaluate sources for Myanmar EFG base map compilation.
-//  * @see https://grouponeo.maps.arcgis.com/home/item.html?id=9ffa28193e8d48b28a584261df6216d2
-//  */
-// export const WEB_MAP_ID = '9ffa28193e8d48b28a584261df6216d2';
-
 export const APP_ID = 'caF3vMbpRnFvWhZJ';
 
 export const TITLE_OF_MYANMAR_EPG_LAYER =
